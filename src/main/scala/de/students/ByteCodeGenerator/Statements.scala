233a211c729099bab0dfe7130dc61b187047c0d8
BitDragon256 <bergben44@gmail.com>
2025-01-08 17:27:39 +0100

    if statement and simple test in main

--- src/main/scala/de/students/ByteCodeGenerator/Statements.scala
+++ src/main/scala/de/students/ByteCodeGenerator/Statements.scala
@@ -1,9 +1,15 @@
 package de.students.ByteCodeGenerator
 
-import org.objectweb.asm.MethodVisitor
+import org.objectweb.asm.{Label, MethodVisitor}
 import org.objectweb.asm.Opcodes.*
-
 import de.students.Parser.*
+
+// temporary I hope
+def handleBlock(block: Block, methodVisitor: MethodVisitor, state: MethodGeneratorState): Unit = {
+  block.statements.foreach(stmt => generateStatement(
+    stmt, methodVisitor, state
+  ))
+}
 
 //////////////////////////
 //      STATEMENTS      //
@@ -14,15 +20,16 @@ private def generateStatement(statement: Statement, methodVisitor: MethodVisitor
     case block: BlockStatement => generateBlockStatement(block, methodVisitor, state)
     case returnStatement: ReturnStatement => generateReturnStatement(returnStatement, methodVisitor, state)
     case expressionStatement: StatementExpression => generateExpressionStatement(expressionStatement, methodVisitor, state)
-    case varDecl: VarDecl => ???
-    case ifStatement: IfStatement => ???
-    case whileStatement: WhileStatement => ???
-    case forStatement: ForStatement => ???
-    case doWhileStatement: DoWhileStatement => ???
-    case switchStatement: SwitchStatement => ???
-    case breakStatement: BreakStatement => ???
-    case continueStatement: ContinueStatement => ???
-    case typedStatement: TypedStatement => ???
+    case ifStatement: IfStatement => generateIfStatement(ifStatement, methodVisitor, state)
+    case whileStatement: WhileStatement => generateWhileStatement(whileStatement, methodVisitor, state)
+    case forStatement: ForStatement => generateForStatement(forStatement, methodVisitor, state)
+    case doWhileStatement: DoWhileStatement => generateDoWhileStatement(doWhileStatement, methodVisitor, state)
+    case switchStatement: SwitchStatement => generateSwitchStatement(switchStatement, methodVisitor, state)
+    case breakStatement: BreakStatement => generateBreakStatement(breakStatement, methodVisitor, state)
+    case continueStatement: ContinueStatement => generateContinueStatement(continueStatement, methodVisitor, state)
+    case typedStatement: TypedStatement => generateTypedStatement(typedStatement, methodVisitor, state)
+    case varDecl: VarDecl => generateVariableDeclaration(varDecl, methodVisitor, state)
+    case _ => throw NotImplementedError("unknown statement")
   }
 }
 
@@ -44,11 +51,52 @@ private def generateReturnStatement(statement: ReturnStatement, methodVisitor: M
   }
 }
 
-private def generateIfStatement(statement: IfStatement, methodVisitor: MethodVisitor, state: MethodGeneratorState): Unit = {
+private def generateIfStatement(ifStatement: IfStatement, methodVisitor: MethodVisitor, state: MethodGeneratorState): Unit = {
+  generateExpression(ifStatement.cond, methodVisitor, state)
+  val elseBranch = Label()
+  val end = Label()
 
+  methodVisitor.visitLdcInsn(1)
+  methodVisitor.visitJumpInsn(IFEQ, elseBranch)
+
+  handleBlock(ifStatement.thenBranch, methodVisitor, state)
+
+  methodVisitor.visitJumpInsn(GOTO, end)
+  methodVisitor.visitLabel(elseBranch)
+
+  if (ifStatement.elseBranch.isDefined) {
+    handleBlock(ifStatement.elseBranch.get, methodVisitor, state)
+  }
+
+  methodVisitor.visitLabel(end)
+  methodVisitor.visitInsn(NOP)
 }
 
 private def generateWhileStatement(statement: WhileStatement, methodVisitor: MethodVisitor, state: MethodGeneratorState): Unit = {
+
+}
+
+private def generateForStatement(statement: ForStatement, methodVisitor: MethodVisitor, state: MethodGeneratorState): Unit = {
+
+}
+
+private def generateDoWhileStatement(statement: DoWhileStatement, methodVisitor: MethodVisitor, state: MethodGeneratorState): Unit = {
+
+}
+
+private def generateSwitchStatement(statement: SwitchStatement, methodVisitor: MethodVisitor, state: MethodGeneratorState): Unit = {
+
+}
+
+private def generateBreakStatement(statement: BreakStatement, methodVisitor: MethodVisitor, state: MethodGeneratorState): Unit = {
+
+}
+
+private def generateContinueStatement(statement: ContinueStatement, methodVisitor: MethodVisitor, state: MethodGeneratorState): Unit = {
+
+}
+
+private def generateVariableDeclaration(statement: VarDecl, methodVisitor: MethodVisitor, state: MethodGeneratorState): Unit = {
 
 }
 
--- src/main/scala/de/students/MiniJavaCompiler.scala
+++ src/main/scala/de/students/MiniJavaCompiler.scala
@@ -1,8 +1,6 @@
 package de.students
 
-import de.students.Parser.{Parser, Package}
-import de.students.InputOutput
-import de.students.semantic.SemanticCheck
+import de.students.Parser.*
 
 object MiniJavaCompiler {
 
@@ -17,18 +15,45 @@ object MiniJavaCompiler {
 
     // Run the scanner and parser
     var io = new InputOutput
-    val input = io.getInput(args)
-    Parser.main(input)
+    // val input = io.getInput(args)
+    // Parser.main(input)
 
     // Create the AST from the parse-tree
-    val astProgram = Package("test", List()) // TODO: AST, generated from parse-tree
+    // val astProgram = Package("test", List()) // TODO: AST, generated from parse-tree
 
     // Run the semantic- and type-check
     // val typedAst = SemanticCheck.runCheck(astProgram)
 
     // Translate the typed AST into bytecode
-    // TODO
+    // some testing
+    val testProg = Package("p",
+      List(
+        ClassDecl(
+          "test",
+          "java/lang/Object",
+          false, // isAbstract
+          List(
+            MethodDecl(
+              "foo",
+              false, // static
+              false, // isAbstract
+              IntType,
+              List(),
+              IfStatement(
+                Literal(1),
+                Block(List(ReturnStatement(Some(TypedExpression(Literal(42), IntType))))),
+                Some(Block(List(ReturnStatement(Some(TypedExpression(Literal(-42), IntType)))))),
+              )
+            )
+          ),
+          List(),
+          List() // constructors
+        )
+      )
+    )
 
+    val bytecode = ByteCodeGenerator.generateBytecode(testProg)
+    io.writeToBinFile(bytecode.head, "test.class")
   }
 
 }
