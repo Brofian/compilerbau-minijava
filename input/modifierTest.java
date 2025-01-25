7d5cbe24d545b03312c2e0efe1ee979ac71f62ba
Benedict <bergben44@gmail.com>
2025-01-25 12:40:43 +0100

    test file for modifiers

---
+++ input/modifierTest.java
@@ -0,0 +1,17 @@
+class A {
+    final int a = 3;
+    final int b;
+    public int c;
+    protected int d;
+    final private int e;
+
+    public static void main(String[] args) { return; }
+    private void foo() { return; }
+    private static void bar() { return; }
+    final void theend() { return; }
+    protected void safe() { return; }
+}
+
+abstract class B {
+    abstract void foo();
+}
