488e9009e6d44fc60eb44f2e6bf5123946ee6b31
Jakub <168542356+JakubSchwenkbeck@users.noreply.github.com>
2025-02-02 16:07:35 +0100

    Update and rename operations.Java to operations.java

--- input/operations.java
+++ input/operations.java
@@ -1,4 +1,4 @@
-/*package operations;
+package operations;
 
 class Operations{
         public static void main(String[] args) {
@@ -9,8 +9,10 @@ class Operations{
             int difference = a - b; // Subtraction
             int product = a * b; // Multiplication
             int quotient = a / b; // Division
-            int remainder = a % b; // Modulus
+            // int remainder = a % b; // Modulus
+            // int inverse = -a;
 
+                
             // Comparison operators
             boolean isEqual = (a == b); // Equal to
             boolean isNotEqual = (a != b); // Not equal to
@@ -24,10 +26,10 @@ class Operations{
             boolean y = false;
             boolean andResult = x && y; // Logical AND
             boolean orResult = x || y; // Logical OR
-
+            //boolean negation = !x;
             // Assignment operators
             int c = 5;
-            c += 2; // Equivalent to c = c + 2
+            /* c += 2; // Equivalent to c = c + 2
 
             c -= 1; // Equivalent to c = c - 1
 
@@ -36,7 +38,7 @@ class Operations{
             c /= 2; // Equivalent to c = c / 2
 
             c %= 2; // Equivalent to c = c % 2
-
+            */
             // Mixed example
             int result = (a + b) * (c - 1); // Using arithmetic operators
             boolean isResultValid = (result > 10) && (result < 50); // Using comparison and logical operators
@@ -45,4 +47,4 @@ class Operations{
 
 }
 
- */
+ 
