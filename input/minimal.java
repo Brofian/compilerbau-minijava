b17dff258edcd767ed2863d11cd567112d09998b
Fabian Holzwarth <holzwarth.fabian@freenet.de>
2025-02-21 12:46:52 +0100

    fix: removed accidentally pushed test code

--- input/minimal.java
+++ input/minimal.java
@@ -2,13 +2,4 @@ package de.minimal;
 
 class Minimal {
 
-    public static void main(String[] args) {
-
-        int[] is = new int[10];
-
-        /*int i = 0;
-        String s = args[1 + 0];
-        System.out.println(s);*/
-    }
-
 }
