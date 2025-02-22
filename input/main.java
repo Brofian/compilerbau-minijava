edcc11d868f5efb9b39483a99b782a39ac7db1ad
Fabian Holzwarth <holzwarth.fabian@freenet.de>
2025-02-22 15:33:25 +0100

    chore: remove unnecessary output

--- input/main.java
+++ input/main.java
@@ -1,17 +1,8 @@
 package de.main;
 
-import java.lang.String;
-
 class Main {
 
     public static void main(String[] args) {
-        /*
-        if (args.length < 1) {
-            System.out.println("Tell me something, and i will repeat!");
-            return;
-        }
-        */
-
         System.out.println(args[0]);
     }
 
