1f401a6bb7c9c59118f084b216d08ec7d62e769a
Fabian Holzwarth <holzwarth.fabian@freenet.de>
2025-02-26 18:16:30 +0100

    chore: removed todo and syntactic sugar for better decompilation results

--- src/main/scala/de/students/ByteCodeGenerator/ByteCodeGenerator.scala
+++ src/main/scala/de/students/ByteCodeGenerator/ByteCodeGenerator.scala
@@ -27,7 +27,7 @@ private def generateClassBytecode(classDecl: ClassDecl): ClassBytecode = {
   val classWriter = new ClassWriter(0)
 
   val javaClassName = javaifyClass(classDecl.name)
-  val parent = javaifyClass(classDecl.parent) // "java/lang/Object" // TODO use real parent
+  val parent = javaifyClass(classDecl.parent)
 
   // set class header
   classWriter.visit(
@@ -130,7 +130,7 @@ private def generateMethodBody(
     methodVisitor,
     methodDecl.returnType
   )
-  state.localVariableCount = (if methodDecl.static then 0 else 1) // if method is not static`this` is param #0
+  state.localVariableCount = if methodDecl.static then 0 else 1 // if method is not static`this` is param #0
 
   methodDecl.params.foreach(param => state.addVariable(param.name, param.varType))
 
